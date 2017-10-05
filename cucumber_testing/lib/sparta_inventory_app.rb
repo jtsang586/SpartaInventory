6fb8465694749cfdfd2150b55df82147a6bdc82f
Tech-A03 <tech-a03@tech-a03s-air-2.home>
2017-10-05 20:06:01 +0100

    logs pom implemented

--- cucumber_testing/lib/sparta_inventory_app.rb
+++ cucumber_testing/lib/sparta_inventory_app.rb
@@ -1,9 +1,14 @@
 require_relative 'pages/sparta_inventory_sign_in_page.rb'
+require_relative 'pages/sparta_inventory_logs_page.rb'
 
 class SpartaInventoryApp
 
   def login_page
     SpartaInventorySignInPage.new
+  end
+
+  def logs_page
+    SpartaInventoryLogsPage.new
   end
 
 end
