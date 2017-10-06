fb21b97a7040992c841a4577ef76435eb501e481
Melinda Fernandes <melindafernandes1993@gmail.com>
2017-10-06 15:32:59 +0100

    hooks to run login before each feature

---
+++ cucumber_testing/features/step_definitions/sparta_inventory_checkout_item_step_defs.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the Checkout Item page$/) do
+  # @sparta_inventory_app.nav_bar.click_checkout_item
+end
+
+When(/^I select the valid item details$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+And(/^click checkout$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I have successfully checked out the item$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
